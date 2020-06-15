7b5e498f13082e9373f794fa419da18e7deda385
ecosy <31912697+ecosy@users.noreply.github.com>
2020-06-15 12:57:09 +0900

    Update cloud_function_eoa_asset.py

--- task_01/cloud_function/cloud_function_eoa_asset.py
+++ task_01/cloud_function/cloud_function_eoa_asset.py
@@ -2,11 +2,12 @@ from google.cloud import bigquery
 import pandas
 import json
 
-# Construct a BigQuery client object.
+# bigquery client 인스턴스 생성
 client = bigquery.Client()
 
-
+# 특정 eoa_address에 대하여 bigquery에 쿼리문을 실행하고 리턴 값 받음
 def get_asset_info(eoa_address):
+    # 쿼리문
     query = """
         SELECT ARRAY(
         SELECT AS STRUCT token_id as token_id, token_address as token_address
@@ -14,7 +15,8 @@ def get_asset_info(eoa_address):
         FROM `bigquery-279701.take_home.eoa_tkid_tkaddr`
         WHERE eoa_address LIKE "%s"
         """ % (eoa_address)
-
+    
+    # bigquery에 해당 쿼리문을 실행하고 리턴 값 받아옴
     query_job = client.query(query)  # Make an API request.
     return query_job.result().to_dataframe().to_json()
 
