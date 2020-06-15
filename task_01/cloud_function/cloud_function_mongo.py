6b2e69b5ba6cad13b645a31e2732da26b44e3133
ecosy <31912697+ecosy@users.noreply.github.com>
2020-06-15 12:58:45 +0900

    Update cloud_function_mongo.py

--- task_01/cloud_function/cloud_function_mongo.py
+++ task_01/cloud_function/cloud_function_mongo.py
@@ -2,13 +2,16 @@ import requests
 import pymongo
 import json
 
-OPENSEA_URL = "https://api.opensea.io/api/v1/assets"
-MONGO_CLIENT_URL = "mongodb+srv://ecosy:2112@cluster0-mqgxy.gcp.mongodb.net/"
+OPENSEA_URL = "https://api.opensea.io/api/v1/assets" # opensea.io에 제공하는 asset 검색 api
+MONGO_CLIENT_URL = "mongodb+srv://ecosy:2112@cluster0-mqgxy.gcp.mongodb.net/" # mongodb client 접속 url
 
 def get_data_from_opensea(request):
+    # request로 받은 데이터를 json 형식으로 바꿈
     request_json = request.get_json(silent=True)
     print(request_json)
-
+    
+    # opensea.io에 보낼 파라미터 변수 생성함
+    # token_id, token_address가 핵심 변수임
     params = {"order_direction":"desc", "limit":"50"}
     params['token_ids'] = request_json['value']
     params['asset_contract_address'] = request_json['token_address']
